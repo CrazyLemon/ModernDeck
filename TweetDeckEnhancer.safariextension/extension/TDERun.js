9a2bf4d46198622ff43a704dff36c1747823861f
dangered wolf <ryandolan123@icloud.com>
2015-03-26 18:12:52 -0400

    got working MutationObserver

--- TweetDeckEnhancer.safariextension/extension/TDERun.js
+++ TweetDeckEnhancer.safariextension/extension/TDERun.js
@@ -2,7 +2,8 @@
 // TweetDeck Enhancer
 // Copyright (c) 2015 Dangered Wolf & The TweetDeck Enhancer Open Source Project
 
-function Initialise() {
+function ReloadTheme() {
+    document.querySelector("html").className = document.querySelector("html").className.replace(" tde-dark","").replace(" tde-light","")
     if (document.querySelector("link[title='dark'][disabled]") !== null) {
         document.querySelector("html").className += " tde-light";
     } else {
@@ -12,8 +13,10 @@ function Initialise() {
 
 var observer = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
-    console.log(mutation.type);
+    ReloadTheme();
   });    
 });
 
-Initialise();
+observer.observe(document.querySelector("link[title='dark']"), {attributes:true,childList:true,characterData:true});
+
+ReloadTheme();
