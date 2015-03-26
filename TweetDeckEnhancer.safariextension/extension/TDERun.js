92d11a2a522a8d3b2e06e3dc6f3c42e5ff48c7d4
dangered wolf <ryandolan123@icloud.com>
2015-03-26 18:04:29 -0400

    working on more things now

--- TweetDeckEnhancer.safariextension/extension/TDERun.js
+++ TweetDeckEnhancer.safariextension/extension/TDERun.js
@@ -2,51 +2,18 @@
 // TweetDeck Enhancer
 // Copyright (c) 2015 Dangered Wolf & The TweetDeck Enhancer Open Source Project
 
-// object.watch
-
 function Initialise() {
-    if (!Object.prototype.watch) {
-        Object.prototype.watch = function (prop, handler) {
-            var oldval = this[prop], newval = oldval,
-            getter = function () {
-                return newval;
-            },
-            setter = function (val) {
-                oldval = newval;
-                console.log(prop);
-                console.log(val);
-                return newval = handler.call(this, prop, oldval, val);
-            };
-            if (delete this[prop]) { // can't watch constants
-                if (Object.defineProperty) { // ECMAScript 5
-                    Object.defineProperty(this, prop, {
-                        get: getter,
-                        set: setter
-                    });
-                } else if (Object.prototype.__defineGetter__ && Object.prototype.__defineSetter__) { // legacy
-                    Object.prototype.__defineGetter__.call(this, prop, getter);
-                    Object.prototype.__defineSetter__.call(this, prop, setter);
-                }
-            }
-        };
-    }
-
-    // object.unwatch
-
-    if (!Object.prototype.unwatch) {
-        Object.prototype.unwatch = function (prop) {
-            var val = this[prop];
-            delete this[prop]; // remove accessors
-            this[prop] = val;
-        };
-    }
-
-    if (document.querySelector("link[title='dark']")) {
-        document.querySelector("link[title='dark']").watch("disabled",function(dark) {
-            console.log("uhm");
-            console.log(dark);
-        });
+    if (document.querySelector("link[title='dark'][disabled]") !== null) {
+        document.querySelector("html").className += " tde-light";
+    } else {
+        document.querySelector("html").className += " tde-dark";
     }
 }
+
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    console.log(mutation.type);
+  });    
+});
 
 Initialise();
