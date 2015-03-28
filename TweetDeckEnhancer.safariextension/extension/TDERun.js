b1a74e6f2f73c0701b9e25828e8254ab1bfb33f0
dangered wolf <ryandolan123@icloud.com>
2015-03-27 21:44:54 -0400

    modal stuffs

--- TweetDeckEnhancer.safariextension/extension/TDERun.js
+++ TweetDeckEnhancer.safariextension/extension/TDERun.js
@@ -4,10 +4,13 @@
 
 function ReloadTheme() {
     document.querySelector("html").className = document.querySelector("html").className.replace(" tde-dark","").replace(" tde-light","")
+    document.querySelector(".application").className = document.querySelector(".application").className.replace(" tde-dark","").replace(" tde-light","")
     if (document.querySelector("link[title='dark'][disabled]") !== null) {
         document.querySelector("html").className += " tde-light";
+        document.querySelector(".application").className += " tde-light";
     } else {
         document.querySelector("html").className += " tde-dark";
+        document.querySelector(".application").className += " tde-dark";
     }
 }
 
